56737529f2bfaf7f97a8eb6482260c830914a8d2
Fredrick Mwangi <fredmallin49@gmail.com>
2025-08-27 10:54:50 +0300

    initialize my project

--- car_service_booking/database.py
+++ car_service_booking/database.py
@@ -3,3 +3,16 @@ import sqlite3
 def get_connection():
     return sqlite3.connect("car_service.db")
 
+def setup_database():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    # Customers Table
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS customers (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            phone TEXT,
+            email TEXT
+        )
+    """)
